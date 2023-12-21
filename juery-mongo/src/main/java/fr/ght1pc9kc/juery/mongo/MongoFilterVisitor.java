f21ff42dd62606a7707202ef54607383ee85d5e4
yyavo <yyavo@botdesign.net>
2023-12-21 17:02:58 +0100

    feat(mongo): add property mapping

--- juery-mongo/src/main/java/fr/ght1pc9kc/juery/mongo/MongoFilterVisitor.java
+++ juery-mongo/src/main/java/fr/ght1pc9kc/juery/mongo/MongoFilterVisitor.java
@@ -12,16 +12,18 @@ import fr.ght1pc9kc.juery.api.filter.LowerThanOperation;
 import fr.ght1pc9kc.juery.api.filter.NoCriterion;
 import fr.ght1pc9kc.juery.api.filter.NotOperation;
 import fr.ght1pc9kc.juery.api.filter.OrOperation;
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 import org.bson.conversions.Bson;
 import org.bson.types.ObjectId;
 
 import java.util.List;
+import java.util.Map;
 
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class MongoFilterVisitor implements CriteriaVisitor<Bson> {
 
     private final List<String> idsFields;
+    private final Map<String, String> propertiesMapping;
 
     @Override
     public Bson visitNoCriteria(NoCriterion none) {
@@ -51,53 +53,59 @@ public class MongoFilterVisitor implements CriteriaVisitor<Bson> {
 
     @Override
     public <T> Bson visitEqual(EqualOperation<T> operation) {
+        String mappedProperty = propertiesMapping.get(operation.field.property);
         if (idsFields.contains(operation.field.property)) {
-            return Filters.eq(operation.field.property, new ObjectId(String.valueOf(operation.value.value)));
+            return Filters.eq(mappedProperty, new ObjectId(String.valueOf(operation.value.value)));
         }
-        return Filters.eq(operation.field.property, operation.value.value);
+        return Filters.eq(mappedProperty, operation.value.value);
     }
 
     @Override
     public <T> Bson visitGreaterThan(GreaterThanOperation<T> operation) {
+        String mappedProperty = propertiesMapping.get(operation.field.property);
         if (idsFields.contains(operation.field.property)) {
-            return Filters.gt(operation.field.property, new ObjectId(String.valueOf(operation.value.value)));
+            return Filters.gt(mappedProperty, new ObjectId(String.valueOf(operation.value.value)));
         }
-        return Filters.gt(operation.field.property, operation.value.value);
+        return Filters.gt(mappedProperty, operation.value.value);
     }
 
     @Override
     public <T> Bson visitGreaterThanEquals(GreaterThanEqualsOperation<T> operation) {
+        String mappedProperty = propertiesMapping.get(operation.field.property);
         if (idsFields.contains(operation.field.property)) {
-            return Filters.gte(operation.field.property, new ObjectId(String.valueOf(operation.value.value)));
+            return Filters.gte(mappedProperty, new ObjectId(String.valueOf(operation.value.value)));
         }
-        return Filters.gte(operation.field.property, operation.value.value);
+        return Filters.gte(mappedProperty, operation.value.value);
     }
 
     @Override
     public <T> Bson visitLowerThan(LowerThanOperation<T> operation) {
+        String mappedProperty = propertiesMapping.get(operation.field.property);
         if (idsFields.contains(operation.field.property)) {
-            return Filters.lt(operation.field.property, new ObjectId(String.valueOf(operation.value.value)));
+            return Filters.lt(mappedProperty, new ObjectId(String.valueOf(operation.value.value)));
         }
-        return Filters.lt(operation.field.property, operation.value.value);
+        return Filters.lt(mappedProperty, operation.value.value);
     }
 
     @Override
     public <T> Bson visitLowerThanEquals(LowerThanEqualsOperation<T> operation) {
+        String mappedProperty = propertiesMapping.get(operation.field.property);
         if (idsFields.contains(operation.field.property)) {
-            return Filters.lte(operation.field.property, new ObjectId(String.valueOf(operation.value.value)));
+            return Filters.lte(mappedProperty, new ObjectId(String.valueOf(operation.value.value)));
         }
-        return Filters.lte(operation.field.property, operation.value.value);
+        return Filters.lte(mappedProperty, operation.value.value);
     }
 
     @Override
     public <T> Bson visitIn(InOperation<T> operation) {
+        String mappedProperty = propertiesMapping.get(operation.field.property);
         if (idsFields.contains(operation.field.property)) {
             List<ObjectId> list = operation.value.value.stream()
                     .map(value -> new ObjectId(String.valueOf(value)))
                     .toList();
-            return Filters.in(operation.field.property, list);
+            return Filters.in(mappedProperty, list);
         }
-        return Filters.in(operation.field.property, operation.value.value);
+        return Filters.in(mappedProperty, operation.value.value);
     }
 
 }
